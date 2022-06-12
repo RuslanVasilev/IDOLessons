d1c753647d49954bef3aa731243889980539a90c
RuslanVasilev <rozinrus@gmail.com>
2022-06-12 23:10:22 +0300

    Lesson2.1

--- src/Lesson2/Lesson2.java
+++ src/Lesson2/Lesson2.java
@@ -1,4 +1,22 @@
 package Lesson2;
 
 public class Lesson2 {
+    public static void main(String[] args) {
+      inputTwoNumbers();
+    }
+
+    public static void inputTwoNumbers() {
+        int a = 9;
+        int b = 5;
+        int c = a + b;
+        if (c >= 10 && c <= 20) {
+
+            System.out.println("True");
+
+
+        }  else if (c < 10 && c >20){
+            System.out.println("False");
+        }
+
+    }
 }
